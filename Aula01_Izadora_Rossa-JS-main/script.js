eb4eea271714bd78185c3f77854dd6ca0780302c
negao666 <gabrielrossa336@gmail.com>
2024-09-18 09:52:46 -0300

    Add files via upload

--- Aula01_Izadora_Rossa-JS-main/script.js
+++ Aula01_Izadora_Rossa-JS-main/script.js
@@ -6,3 +6,10 @@ var nome = prompt("Qual seu nome? ");
 var sobrenome = prompt("Qual seu sobrenome? ");
 
 console.log("Olá " + nome + " " + sobrenome);
+
+//saida do dom
+
+document.write("<h1>Bem vindo " +nome+ " " +sobrenome+  "</h1>");
+document.write("Steve atraves do javascript");
+
+document.write("<img src=https://sujeitoprogramador.com/steve.png alt=foto do Steve>");
